d8679eb540a8d7d4cd9b900a2c87d29d65f1c767
Nimitha-jagadeesha <nimitha1jagadeesha@gmail.com>
2020-06-21 00:45:33 +0530

    single page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -22,8 +22,14 @@ class Main extends Component {
       leaders: LEADERS
     };
   }
-
+  
   render() {
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     const HomePage = () => {
       return(
           <Home 
@@ -33,6 +39,7 @@ class Main extends Component {
           />
       );
     }
+  
     return (
       <div>
         <Header />
@@ -44,6 +51,7 @@ class Main extends Component {
             component={() => <Menu dishes={this.state.dishes} />}
           />
           <Route exact path='/contactus' component={Contact} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
